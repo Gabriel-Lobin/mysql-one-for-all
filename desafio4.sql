6cb14dac82bfc7974aa4238f365a4adeac3d6e2d
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-29 18:25:49 -0300

    [requisito 4] resolucao

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+A.artista AS `artista`,
+COUNT(A.artista) AS `seguidores`
+FROM
+SpotifyClone.artistas AS A
+INNER JOIN
+SpotifyClone.seguindo_artista AS SA
+ON SA.artista_id = A.artista_id
+GROUP BY `artista`
+ORDER BY `seguidores` DESC, `artista`;

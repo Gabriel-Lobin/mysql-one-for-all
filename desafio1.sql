4a94229b9ad81a1cd34ceee190ec8658fa045580
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-29 16:52:51 -0300

    [requisito 1] testando

---
+++ desafio1.sql
@@ -0,0 +1,138 @@
+DROP SCHEMA IF EXISTS SpotifyClone;
+
+CREATE SCHEMA SpotifyClone;
+
+USE SpotifyClone;
+    
+CREATE TABLE planos(
+	plano_id INT PRIMARY KEY AUTO_INCREMENT,
+    plano VARCHAR(25) NOT NULL,
+    valor VARCHAR(10) NOT NULL
+);
+	    
+CREATE TABLE artistas(
+ 	artista_id INT PRIMARY KEY AUTO_INCREMENT,
+    artista VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE albums(
+	album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album VARCHAR(30) NOT NULL,
+    artista_id INT NOT NULL,   
+    FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)    
+);
+
+CREATE TABLE musicas(
+	musica_id INT PRIMARY KEY AUTO_INCREMENT,
+    musica VARCHAR(50) NOT NULL,
+    album_id INT NOT NULL,    
+    FOREIGN KEY (album_id) REFERENCES albums (album_id)    
+);
+
+CREATE TABLE usuarios(
+	usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR(40) NOT NULL,
+    idade INT NOT NULL,
+    plano_id INT NOT NULL,    
+    FOREIGN kEY (plano_id) REFERENCES planos (plano_id)    
+);
+
+CREATE TABLE historicos(
+	usuario_id INT NOT NULL,
+    musica_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (usuario_id, musica_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios (usuario_id),
+    FOREIGN KEY (musica_id) REFERENCES musicas (musica_id)    
+);
+
+CREATE TABLE seguindo_artista(
+ 	usuario_id INT NOT NULL,
+    artista_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (usuario_id, artista_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios (usuario_id),
+    FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)    
+);
+
+
+INSERT INTO planos (plano, valor)
+VALUES
+('gratuito', '0'),
+('universitário', '5,99'),
+('familiar', '7,99');
+
+INSERT INTO artistas (artista)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
+
+INSERT INTO albums (album, artista_id)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO musicas (musica, album_id)
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO usuarios (usuario, idade, plano_id)
+VALUES
+('Thati', 23, 1),
+('Cintia', 35, 3),
+('Bill', 20, 2),
+('Roger', 45, 1);
+
+INSERT INTO historicos (usuario_id, musica_id)
+VALUES
+(1,1),
+(1,6),
+(1,14),
+(1,16),
+(2,13),
+(2,17),
+(2,2),
+(2,15),
+(3,4),
+(3,16),
+(3,6),
+(4,3),
+(4,18),
+(4,11);
+
+INSERT INTO seguindo_artista (usuario_id, artista_id)
+VALUES
+(1,1),
+(1,4),
+(1,3),
+(2,1),
+(2,3),
+(3,2),
+(3,3),
+(4,4);
+
+
+
+
+
+

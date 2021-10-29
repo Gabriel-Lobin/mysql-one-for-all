d6d0197fa8ec729d359d60bb3a688d6274bb5475
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-29 20:14:57 -0300

    [requisito 6] correcao

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-FORMAT(MIN(P.valor),2) AS `faturamento_minimo`,
+FORMAT(MIN(P.valor), 2) AS `faturamento_minimo`,
 FORMAT(MAX(P.valor), 2) AS `faturamento_maximo`,
 FORMAT(AVG(P.valor), 2) AS `faturamento_medio`,
-FORMAT(SUM(P.valor),2) AS `faturamento_total`
+FORMAT(SUM(P.valor), 2) AS `faturamento_total`
 FROM
 SpotifyClone.planos AS P
 INNER JOIN

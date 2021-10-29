8c699aea6357a4e15f1ff22f73b7fadad3d40f7a
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-29 18:56:18 -0300

    [requisito 6] resolucao

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+CREATE VIEW faturamento_atual AS
+SELECT
+ROUND(MIN(P.valor),2) AS `faturamento_minimo`,
+ROUND(MAX(P.valor), 2) AS `faturamento_maximo`,
+ROUND(AVG(P.valor), 2) AS `faturamento_medio`,
+ROUND(SUM(P.valor),2) AS `faturamento_total`
+FROM
+SpotifyClone.planos AS P
+INNER JOIN
+SpotifyClone.usuarios AS U
+ON P.plano_id = U.plano_id;

be8a2519f247335b4aa1cfab81b10726de44a21f
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-30 17:18:22 -0300

    [requisito 8] correcao

--- desafio8.sql
+++ desafio8.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER trigger_usuario_delete
 	BEFORE DELETE ON usuarios
 	FOR EACH ROW
 BEGIN
-    DELETE FROM historicos AS H WHERE OLD.usuario_id = H.usuario_id;
-	DELETE FROM seguindo_artista AS A WHERE OLD.usuario_id = A.usuario_id;
+    DELETE FROM historicos AS H WHERE usuarios.usuario_id = H.usuario_id;
+	DELETE FROM seguindo_artista AS A WHERE usuarios.usuario_id = A.usuario_id;
 END &&
 DELIMITER ;

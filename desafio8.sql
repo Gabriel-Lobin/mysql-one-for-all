0f5d70af370d5ec6785db2bb547dbd327dd24ea7
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-10-30 14:35:32 -0300

    [requisito 8] resolucao teste

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+DROP TRIGGER IF EXISTS trigger_usuario_delete;
+DELIMITER &&
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON usuarios
+FOR EACH ROW
+BEGIN
+    DELETE FROM historicos AS H WHERE OLD.usuario_id = H.usuario_id;
+	DELETE FROM seguindo_artista AS A WHERE OLD.usuario_id = A.usuario_id;
+END &&
+DELIMITER ;

8d6eb85317347cc358f45256bee392385c4ec78c
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-03 12:48:55 -0300

    [requisito 8] correcao

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ DROP TRIGGER IF EXISTS trigger_usuario_delete;
 DELIMITER &&
 
 CREATE TRIGGER trigger_usuario_delete
-	BEFORE DELETE ON usuarios
+	BEFORE DELETE ON SpotifyClone.usuarios
 	FOR EACH ROW
 BEGIN
     DELETE FROM historicos AS H WHERE OLD.usuario_id = H.usuario_id;
